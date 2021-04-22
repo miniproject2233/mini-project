4d231cc71bc4879c4e1062af05b42e350f16f6a1
miniproject2233 <80024398+miniproject2233@users.noreply.github.com>
2021-04-22 16:39:22 +0530

    Update settings.py

--- sentimentAnalysis/settings.py
+++ sentimentAnalysis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9nb4!%=dmwn&s2zl3ar@ah&+=5mrmkxbh1&8!4sqd-8u*dfq6w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','sentiment-analysis-with-bot.herokuapp.com']
 
 
 # Application definition
